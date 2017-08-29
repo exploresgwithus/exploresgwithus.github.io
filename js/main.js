b4c075bd87a5fc5c0e24a6e350b0bdb07db21e2b
EversoundAsia <eversoundasia@gmail.com>
2017-08-30 03:06:50 +0800

    Upload3

--- js/main.js
+++ js/main.js
@@ -119,7 +119,9 @@ jQuery(document).ready(function () {
 /*preloder*/
 $(window).load(function() { // makes sure the whole site is loaded
 	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#loader-wrapper').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('#
+
+        - wrapper').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	$('body').delay(200).css({'overflow-x':'hidden'});
 })	
 	
